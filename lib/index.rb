ff32621d2b6e24cd5eea7aacd88ca99837a1425d
415CA <chrisallen415@gmail.com>
2020-06-16 16:58:07 -0400

    add methods for all of the loops

--- lib/index.rb
+++ lib/index.rb
@@ -17,13 +17,108 @@ library = [
 ]
 
 # WRITE CODE BELOW HERE
+def all_pages(library)
+  library.reduce(0) {|sum, book| sum += book[:pages]}
+end
 
+puts all_pages(library)
 
+def all_pages_read(library)
+  library.reduce(0) { |sum, book| book[:completed] ? sum += book[:pages] : sum}
+end
 
+puts all_pages_read(library)
+
+def all_genres(library)
+  library.reduce([]) {|arr, book| arr += book[:genres]}.flatten.uniq
+end
+
+puts all_genres(library)
+
+def books_read(library)
+  library.reduce([]) { |arr, book| book[:completed] ? arr << book : arr }
+end
+
+puts books_read(library)
+
+def completed_books_detail(library)
+  puts "I've read #{books_read(library).length} books, totaling at #{all_pages_read(library)} pages."
+end
+
+puts completed_books_detail(library)
+
+def books_in_library(library)
+  library.reduce([]) { |arr, book| arr.push("#{book[:title]} by #{book[:author]}")}
+end
+
+puts books_in_library(library)
+
+def books_by_year(library)
+  library = library.sort do |book_a, book_b| book_a[:publication_year] - book_b[:publication_year]
+  end
+  books_in_library(library)
+end
+
+puts books_by_year(library)
+
+def books_with_subtitles(library)
+  library.reduce([]) { |arr, book| book[:title].include?((/;|:/) ? arr << book : arr }
+end
+
+puts books_with_subtitles(library)
+
+def books_primary_title(library)
+  books_with_subtitles(library).reduce([]) {|arr, book| arr << book[:title].split(/;|:/).first}
+end
+
+puts books_primary_title(library)
+
+def highest_page_count(library)
+  library.sort {|book_a, book_b| book_b[:pages] - book_a[:pages] }.first
+end
+
+puts highest_page_count(library)
+
+def recommended_books(library, number)
+  library = books_read(library).sort {|book_a, book_b| book_a[:pages] - book_b[:pages]}
+  array = []
+  index = 0
+  while index < number do
+    book = books_read(library)[index]
+    array.push("You should read #{book[:title]} by #{book[:author]}; it's only #{book[:pages]} pages long!")
+    index += 1
+  end
+  array
+end
+
+puts recommended_books(library, 3)
+
+def recommended_books_by_genre(library, prefered_genres, rejected_genres)
+  recommended = library.select { |book| book[:genres].include?(prefered_genres) }
+  additional = library.select { |book|
+    !book[:genres].include?(prefered_genres) && !book[:genres].include?(rejected_genres)
+  }
+  output = []
+  index = 0
+  while index < recommended.length
+    book = recommended[index]
+    output.push("Since you like #{prefered_genres}, you should read #{book[:title]} by #{book[:author]}!")
+    index += 1
+  end
+
+  if index == recommended.length
+    add_index = 0
+    while add_index < additional.length
+      book = additional[add_index]
+      output.push("I also recommend #{book[:title]} by #{book[:author]}.")
+      add_index += 1
+      binding.pry
+    end
+  end
+  output
+end
+
+puts recommended_books_by_genre(library, "Historical Fiction", "Science Fiction")
 
 # WRITE CODE ABOVE HERE
-
-
-binding.pry
-
 puts "Books!"
